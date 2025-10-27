78e17befce9f25cf956b443a3ad693e570b160ab
alooonsogm <alonsogarciamartin2@gmail.com>
2025-10-27 14:14:40 +0100

    27/10/2025

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <PadreDeporte/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PadreDeporte from './components/PadreDeporte.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PadreDeporte
   }
 }
 </script>
---
+++ src/components/PadreDeporte.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1 style="color:blue">Padre deportes</h1>
+        <ul>
+            <li v-for="deport in deportes" :key="deport">
+                <HijoDeporte :nombredeporte="deport"/>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import HijoDeporte from './HijoDeporte.vue'
+    export default{
+        name: "PadreDeporte",
+        components: {
+            HijoDeporte
+        },
+        data(){
+            return {
+                deportes: ["Curling", "Futbol", "Bolos", "Padel"],
+            }
+        }
+    }
+</script>
