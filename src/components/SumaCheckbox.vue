1c4d87388ce6757f96ad6377a16202e6745813e4
alooonsogm <alonsogarciamartin2@gmail.com>
2025-10-28 17:50:29 +0100

    28/10/2025 - casa

--- src/components/SumaCheckbox.vue
+++ src/components/SumaCheckbox.vue
@@ -4,11 +4,12 @@
         <form>
             <button @click.prevent="generarCheckbox">Generar boton</button>
             <div v-for="num in numeros" :key="num">
-                <input type="checkbox" :value="num"/>
+                <input type="checkbox" :value="num" v-model="checkboxSelected" @change.prevent="hacerSuma()"/>
                 <label>{{ num }}</label>
             </div>
         </form>
         <h3>La suma es: {{ laSuma }}</h3>
+        <h3> {{ checkboxSelected }}</h3>
     </div>
 </template>
 
@@ -19,14 +20,18 @@
             return{
                 numeros: [parseInt(Math.random() * 100 + 1), parseInt(Math.random() * 100 + 1), parseInt(Math.random() * 100 + 1)],
                 laSuma: 0,
+                checkboxSelected: [],
             }
         },
         methods: {
             generarCheckbox(){
                 this.numeros.push(parseInt(Math.random() * 100 + 1));
             },
-            hacerSuma(numero){
-                this.laSuma = this.laSuma + parseInt(numero)
+            hacerSuma(){
+                this.laSuma = 0;
+                for(var num of this.checkboxSelected){
+                    this.laSuma += num;
+                }
             }
         }
     }
